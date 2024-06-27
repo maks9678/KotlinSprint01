2d95508cdd9ea1dcb34963f5c3d96773e7e0a8aa
Maksim Medvedev <maksim.9678@mail.ru>
2024-06-28 00:04:22 +0300

    KS-1-1 refactor

--- src/main/kotlin/lesson_1/lesson1_task1.kt
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -1,7 +1,6 @@
 package org.example.lesson_1
 
-fun main(){
-    val numberOfOrders:Int = 75 //количество заказов
-    val thanksShopper:String = "Спасибо, за выбор нашего магазина!" //текст с благодарностью за покупку
-
+fun main() {
+    val numberOfOrders:Int = 75
+    val thanksShopper:String = "Спасибо, за выбор нашего магазина!"
 }
