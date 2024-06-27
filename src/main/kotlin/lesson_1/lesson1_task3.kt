047f832ba5a8d3f7fb693f75c3923b4b018a87d6
Maksim Medvedev <maksim.9678@mail.ru>
2024-06-28 00:11:07 +0300

    KS-1-3 refactor

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,13 +1,13 @@
 package org.example.lesson_1
 
-fun main(){
+fun main() {
     val year = 1961
-    var hour = 9
-    var minute = 7
+    var hour = "09"
+    var minute = "07"
     println(year)
     println(hour)
     println(minute)
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
     println("$hour:$minute")
 }
