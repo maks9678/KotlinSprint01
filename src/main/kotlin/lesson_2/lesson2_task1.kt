eedc12426a4bf1beb4604224cef7a6cb801d553b
Maksim Medvedev <maksim.9678@mail.ru>
2024-06-28 01:34:24 +0300

    KS-2-1 gpa

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_2
+
+fun main() {
+    val student1 = 3
+    val student2 = 4
+    val student3 = 3
+    val student4 = 5
+
+    var gpa = (student1 + student2 + student3 + student4) / 4f
+
+    print(gpa)
+}
