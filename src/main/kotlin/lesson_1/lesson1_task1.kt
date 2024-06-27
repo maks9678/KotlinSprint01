d86e7770f6d1068067509fe6b5d86c6807b5821e
Maksim Medvedev <maksim.9678@mail.ru>
2024-06-27 22:15:00 +0300

    KS-1-1 variables are named

---
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -0,0 +1,7 @@
+package org.example.lesson_1
+
+fun main(){
+    val numberOfOrders:Int = 75 //количество заказов
+    val thanksShopper:String = "Спасибо, за выбор нашего магазина!" //текст с благодарностью за покупку
+
+}
