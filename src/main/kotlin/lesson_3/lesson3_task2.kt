d526c6fb77b77ff3107dab9c1c743b07db876b3b
Maksim Medvedev <maksim.9678@mail.ru>
2024-06-30 01:22:51 +0300

    KS-3-2

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_3
+
+fun main() {
+
+    var fullName = "Андреева Татьяна Сергеевна"
+    var age = 20
+
+    println("$fullName, $age")
+
+    fullName = "Сидорова Татьяна Сергеевна"
+    age = 22
+
+    println("$fullName, $age")
+
+}
