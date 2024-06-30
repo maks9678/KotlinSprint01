8d94921e630a3dc9c45a2b14adaca4cc7faa8deb
Maksim Medvedev <maksim.9678@mail.ru>
2024-06-30 22:55:28 +0300

    KS-2-4 refactor

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -2,12 +2,13 @@ package org.example.lesson_2
 
 fun main() {
 
-    val buff = 0.2
+    val buff = 20
     val crystalOre = 7
     val ironOre = 11
+    val percentageInNumber = 100
 
-    val buffCristalOne = (crystalOre * buff).toInt()
-    val buffIronOne = (ironOre * buff).toInt()
+    val buffCristalOne = (crystalOre * buff / percentageInNumber).toInt()
+    val buffIronOne = (ironOre * buff / percentageInNumber).toInt()
 
     println("Кристалической руды $buffCristalOne")
     println("Железной руды $buffIronOne")
