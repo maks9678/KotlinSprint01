77f4b705d6027a7e83558556f3883ba1f180522d
Maksim Medvedev <maksim.9678@mail.ru>
2024-06-30 22:12:10 +0300

    KS-2-2 refactor

---
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_2
+
+fun main() {
+
+    val numberOfEmployees = 50
+    val salaryOfEmployees = 30000
+    val numberOfInterns = 30
+    val salaryOfInterns = 20000
+
+    val totalSalaryOfEmployees = numberOfEmployees * salaryOfEmployees
+    val totalSalaryOfInterns = numberOfInterns * salaryOfInterns
+
+    val totalSalary = totalSalaryOfEmployees + totalSalaryOfInterns
+
+    val numberEmployeesAndInterns = numberOfEmployees + numberOfInterns
+
+    val averageSalary = totalSalary / numberEmployeesAndInterns
+
+}
