08336d2de3b8b1be6118ef24e2922a03a31a6251
Maksim Medvedev <maksim.9678@mail.ru>
2024-07-01 02:06:42 +0300

    KS-1-5 time

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,7 +1,15 @@
 package org.example.lesson_1
 
 fun main() {
+
     val sec = 6480
-    val minuts =
+    val timeConverter = 60
+
+    val horus = sec / timeConverter / timeConverter
+    val minuts = sec / timeConverter % 60
+    val secRemains = sec % timeConverter % timeConverter
+
+    println("$horus:$minuts:$secRemains")
+
 
 }
