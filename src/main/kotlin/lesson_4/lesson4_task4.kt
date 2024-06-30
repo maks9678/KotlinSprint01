bcf3716ba7f85b7cd22a946548f4104b1c43b2ab
Maksim Medvedev <maksim.9678@mail.ru>
2024-07-01 00:35:18 +0300

    KS-4-4 trainingDay

---
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_4
+
+fun main() {
+
+    val trainingDay = 5
+
+    val trainingDeterminant = 5 % 2 == 1
+
+    println("Упражнения для рук: $trainingDeterminant\nУпражнения для ног: ${!trainingDeterminant}\nУпражнения для спины: ${!trainingDeterminant}\nУпражнения для пресса: $trainingDeterminant")
+
+}
