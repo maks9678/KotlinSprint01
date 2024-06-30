b9c97ebfa5786376885c013509962ce60588310e
Maksim Medvedev <maksim.9678@mail.ru>
2024-06-30 22:50:43 +0300

    KS-2-3 refactor

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -2,27 +2,18 @@ package org.example.lesson_2
 
 fun main() {
 
-    var departureTime: List<String> = "9:39".split(":")
-    var travelTimeMinuts = 457
+    val departureTime: List<String> = "9:39".split(":")
+    val travelTimeMinuts = 457
+    val minutesPerHour = 60
 
-    val trevelTimeHorus = travelTimeMinuts / 60
-    travelTimeMinuts = travelTimeMinuts % 60
+    val trevelTimeHorus = travelTimeMinuts / minutesPerHour
+    val remainingTrevelMinutes = travelTimeMinuts % minutesPerHour
 
-    var arrivalTime = arrayOf(0,0)
+    val arrivalHours = departureTime[0].toInt() + trevelTimeHorus
+    val arrivalMinutes = departureTime[1].toInt() + travelTimeMinuts % minutesPerHour
 
-    arrivalTime[0] = departureTime[0].toInt() + trevelTimeHorus
-    arrivalTime[1] = departureTime[1].toInt() + travelTimeMinuts
+    println("${arrivalHours + arrivalMinutes / minutesPerHour}:${arrivalMinutes % minutesPerHour}")
 
-    if(arrivalTime[1] > 60){
-        arrivalTime[0]+=1
-        arrivalTime[1]-=60
-    }
-    else if(arrivalTime[1]==60){
-        arrivalTime[0]+=1
-        arrivalTime[1]=0
-    }
-
-    print("${arrivalTime[0]}:${arrivalTime[1]}")
 }
 
 
