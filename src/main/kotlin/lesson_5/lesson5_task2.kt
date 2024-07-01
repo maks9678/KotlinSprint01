954c822468f90648777094abeb9a528e48abc78b
Maksim Medvedev <maksim.9678@mail.ru>
2024-07-02 00:01:58 +0300

    KS-5-2 userAge

---
+++ src/main/kotlin/lesson_5/lesson5_task2.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_5
+
+const val AGE_OF_MAJORITY = 18
+
+fun main() {
+
+    val currentYear = 2024
+    println("Введите свой год рождения:")
+    val yearOfBirth = readLine()!!.toInt()
+
+    val userAge = currentYear - yearOfBirth
+
+    if (userAge >= AGE_OF_MAJORITY) println("Показать экран со скрытым контентом")
+    else println("Придется подрасти")
+
+}
