177f8376775fcee6fa48d5f6d97d73054d88e877
Maksim Medvedev <maksim.9678@mail.ru>
2024-07-01 22:52:15 +0300

    KS-3-5 refactor

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -2,10 +2,10 @@ package org.example.lesson_3
 
 fun main() {
 
-    val recordingMove = "D2-D4;0".split("")
+    val recordingMove = "D2-D4;0"
 
-    val fromMotion = recordingMove[1]+recordingMove[2]
-    val whereMotion = recordingMove[4]+recordingMove[5]
+    val fromMotion = recordingMove[1] + recordingMove[2]
+    val whereMotion = recordingMove[4] + recordingMove[5]
     val numerMotion = recordingMove[7]
 
     println(fromMotion)
