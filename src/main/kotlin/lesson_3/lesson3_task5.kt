1910dd3c0df26d64e6a11a6c66412f77c130170d
Maksim Medvedev <maksim.9678@mail.ru>
2024-07-01 12:51:28 +0300

    KS-3-5 Parse the string

---
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_3
+
+fun main() {
+
+    val recordingMove = "D2-D4;0".split("")
+
+    val fromMotion = recordingMove[1]+recordingMove[2]
+    val whereMotion = recordingMove[4]+recordingMove[5]
+    val numerMotion = recordingMove[7]
+
+    println(fromMotion)
+    println(whereMotion)
+    println(numerMotion)
+
+}
