688eaf25be96162afbf15646dc1afc64a41f1562
Maksim Medvedev <maksim.9678@mail.ru>
2024-07-02 23:40:00 +0300

    KS-1-5 refactoring 2

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,15 +1,15 @@
 package org.example.lesson_1
 
+const val timeDivider = 60
+
 fun main() {
 
     val sec = 6480
-    val timeConverter = 60
 
-    val horus = sec / timeConverter / timeConverter
-    val minuts = sec / timeConverter % 60
-    val secRemains = sec % timeConverter % timeConverter
+    val horus = sec / timeDivider / timeDivider
+    val minuts = sec / timeDivider % 60
+    val secRemains = sec % timeDivider % timeDivider
 
     println("${"%02d".format(horus)}:${"%02d".format(minuts)}:${"%02d".format(secRemains)}")
-
 
 }
