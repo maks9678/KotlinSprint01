01e938d953a274243cec07aabce5a048d6dde66b
Maksim Medvedev <maksim.9678@mail.ru>
2024-07-03 01:49:41 +0300

    KS-5-5 refactoring

--- src/main/kotlin/lesson_5/lesson5_task5.kt
+++ src/main/kotlin/lesson_5/lesson5_task5.kt
@@ -4,18 +4,17 @@ import kotlin.random.Random
 
 fun main() {
 
-    val winningList = List(3) { Random.nextInt(0, 2) }
+    val winningList = List(3) { Random.nextInt(0, 42) }
 
     println("Введите три числа от 0 до 42:")
-    val userNumber = listOf(readLine()!!.toInt(), readLine()!!.toInt(), readLine()!!.toInt())
+    val userNumber = listOf(readln().toInt(), readln().toInt(), readln().toInt())
 
     val numberOfMatches = (winningList.intersect(userNumber)).size
-    if (numberOfMatches == 2) {
-        println("Поздравляем! Вы выиграли главный приз!")
-    } else if (numberOfMatches == 1) {
-        println("Вы выиграли утешительный приз!")
-    } else {
-        println("Неудача!")
+    when (numberOfMatches) {
+        3 -> println("Поздравляем! Вы выиграли главный приз!")
+        2 -> println("Вы выиграли утешительный приз!")
+        1 -> println("Вы выиграли утешительный приз!")
+        0 -> println("Неудача!")
     }
     println("Правильные цифры \n$winningList")
 }
