cff09d09af31764199a92b316439ee56a16e6143
Maksim Medvedev <maksim.9678@mail.ru>
2024-07-02 23:59:41 +0300

    KS-4-2 refactoring

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -1,12 +1,17 @@
 package org.example.lesson_4
 
+val MIN_WEIGHT = 35
+val MAX_WEIGHT = 100
+val MAX_VOLUME = 100
+
 fun main() {
 
-    val minWeight = 35
-    val maxWeight = 100
-    val maxVolume = 100
+    val cargoWeight1 = 20
+    val cargoVolume1 = 80
+    val cargoWeight2 = 50
+    val cargoVolume2 = 100
 
-    println("Груз с весом 20 кг и объемом 80 л соответствует категории 'Average': ${20 > minWeight && 20 <= maxWeight && 80 < maxVolume}")
-    println("Груз с весом 50 кг и объемом 100 л соответствует категории 'Average': ${50 > minWeight && 50 <= maxWeight && 100 < maxVolume}")
+    println("Груз с весом 20 кг и объемом 80 л соответствует категории 'Average': ${cargoWeight1 > MIN_WEIGHT && cargoWeight1 <= MAX_WEIGHT && cargoVolume1 < MAX_VOLUME}")
+    println("Груз с весом 50 кг и объемом 100 л соответствует категории 'Average': ${cargoWeight2 > MIN_WEIGHT && cargoWeight2 <= MAX_WEIGHT && cargoVolume2 < MAX_VOLUME}")
 
 }
