29b0b4face8ad879bf5f0da4b4c20b60def5263e
Maksim Medvedev <maksim.9678@mail.ru>
2024-07-03 21:09:03 +0300

    KS-1-5 refactoring 4

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -7,7 +7,7 @@ fun main() {
     val sec = 6480
 
     val horus = sec / TIME_DIVIDER / TIME_DIVIDER
-    val minuts = sec / TIME_DIVIDER % 60
+    val minuts = sec / TIME_DIVIDER % TIME_DIVIDER
     val secRemains = sec % TIME_DIVIDER % TIME_DIVIDER
 
     println("${"%02d".format(horus)}:${"%02d".format(minuts)}:${"%02d".format(secRemains)}")
