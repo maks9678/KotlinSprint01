b3dc37f3cc20571cf048de29814faa7f83b045d9
Maksim Medvedev <maksim.9678@mail.ru>
2024-07-05 18:26:46 +0300

    KS-7-1 refactor

--- src/main/kotlin/lesson_7/lesson7_task1.kt
+++ src/main/kotlin/lesson_7/lesson7_task1.kt
@@ -1,23 +1,16 @@
 package org.example.lesson_7
 
-import kotlin.random.Random
-
-const val FROM_THE_NUMBER = 0
-const val TO_THE_NUMBER = 9
-const val FROM_THE_LETTER = 'a'
-const val TO_THE_LETTER = 'z'
 const val NUMBER_OF_CHARACTERS = 6
 
 fun main() {
-
-    val lettersToChooseFrom = (FROM_THE_LETTER..TO_THE_LETTER).toList()
+    val charRangeForPassword: CharRange = 'a'..'z'
+    val intRangeForPassword: IntRange = 0..9
     var password = ""
     for (i in 1..NUMBER_OF_CHARACTERS) {
-        if (i % 2 == 1) {
-            val numberLetter = Random.nextInt(0, lettersToChooseFrom.size )
-            password += lettersToChooseFrom[numberLetter]
+        if (i % 2 == 0) {
+            password += intRangeForPassword.random()
         } else {
-            password += Random.nextInt(FROM_THE_NUMBER, TO_THE_NUMBER)
+            password += charRangeForPassword.random()
         }
     }
     println("Сгенерированный пароль: $password")
