e3064bf157013f5a38c91e3ac8b3cb289fcc12fa
Maksim Medvedev <maksim.9678@mail.ru>
2024-07-05 22:29:32 +0300

    KS-8-5 Array(n,{""})

---
+++ src/main/kotlin/lesson_8/lesson8_task5.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_8
+
+fun main() {
+    println("Введите количество ингридиентов")
+    val numberIngredients = readln().toInt()
+    val arrayIngredients = Array(numberIngredients, { "" })
+
+
+    for (i in 0..numberIngredients - 1) {
+        println("Введите ${i + 1} ингридиент")
+        arrayIngredients[i] = readln()
+    }
+    print("Список ингридиентов:")
+    for (i in arrayIngredients) {
+        if (i == arrayIngredients[numberIngredients-1]) print(" $i")
+        else print(" $i,")
+    }
+}
