203d53a5484ffc673cc148d355149514a0697df8
Maksim Medvedev <maksim.9678@mail.ru>
2024-07-07 22:53:31 +0300

    KS-9-4 refactor

--- src/main/kotlin/lesson_9/lesson9_task4.kt
+++ src/main/kotlin/lesson_9/lesson9_task4.kt
@@ -2,8 +2,7 @@ package org.example.lesson_9
 
 fun main() {
     println("Введите названия 5 ингридиетов: ")
-    val listingredientsOfTheDich = readln().split(", ").toMutableList()
-    val sortListIngredientsOfTheDich = listingredientsOfTheDich.sort()
+    val listingredientsOfTheDich: List<String> = readln().split(", ")
+    val sortListIngredientsOfTheDich = listingredientsOfTheDich.sorted()
     println(sortListIngredientsOfTheDich)
-    //sortListIngredientsOfTheDich.forEach { println(it) }
 }
