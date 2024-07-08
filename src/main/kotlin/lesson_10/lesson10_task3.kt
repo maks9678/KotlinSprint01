2525377303110d3520e20b4ed2658accac9c4ee5
Maksim Medvedev <maksim.9678@mail.ru>
2024-07-08 23:55:04 +0300

    KS-10-3 version 2

--- src/main/kotlin/lesson_10/lesson10_task3.kt
+++ src/main/kotlin/lesson_10/lesson10_task3.kt
@@ -11,7 +11,7 @@ fun main() {
 fun passwordGeneration(lengthPassword: Int): String {
     val numericValues = 0..9
     val specialCharacters = "!\"#$%&'()*+,-./ "
-    var password= ""
+    var password = ""
 
     for (i in 1..lengthPassword) {
         if (i % 2 != 0) {
