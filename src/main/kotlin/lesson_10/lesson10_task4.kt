5e3703c9a2cb433a3a72ed3206b4eb7132c4479c
Maksim Medvedev <maksim.9678@mail.ru>
2024-07-10 23:55:04 +0300

    KS-10-4 version 2

--- src/main/kotlin/lesson_10/lesson10_task4.kt
+++ src/main/kotlin/lesson_10/lesson10_task4.kt
@@ -4,37 +4,41 @@ const val MIN_RANDOM_NUMBER = 1
 const val MAX_RANDOM_NUMBER = 6
 
 fun main() {
-    var numberOfWinningGamesOfAPerson = 0
+    var playerWins = 0
     while (true) {
-        numberOfWinningGamesOfAPerson = gameDice()
-        println("Хотите бросить кости еще раз? Введите Да или Нет")
-        var continueOrStopGame = readln()
-        if (continueOrStopGame == "Да") {
+        playerWins = gameDice(playerWins)
+        val continueGame = onceAgain()
+        if (continueGame == "да") {
             continue
         } else {
             break
         }
     }
-    println("Количество побед игрока: $numberOfWinningGamesOfAPerson")
+    println("Количество побед игрока: $playerWins")
 }
 
-fun gameDice(): Int {
+fun gameDice(playerWins: Int): Int {
     println("Игрок бросает кости")
     val playerResult: Int = rollOfTheDice()
     println("Игроку выпало: $playerResult ")
-
+    var counterPlayerWins = playerWins
     println("Компьютер бросает кости")
     val computerResult: Int = rollOfTheDice()
     println("Компьютеру выпало: $computerResult ")
-    var counterOfWinningGamesOfAPerson = 0
     if (playerResult > computerResult) {
         println("Победило человечество")
-        counterOfWinningGamesOfAPerson++
+        counterPlayerWins++
     } else if (playerResult == computerResult) println("Ничья")
     else println("Победила машина")
-    return counterOfWinningGamesOfAPerson
+    return counterPlayerWins
 }
 
 fun rollOfTheDice(): Int {
     return (MIN_RANDOM_NUMBER..MAX_RANDOM_NUMBER).random()
+}
+
+fun onceAgain(): String {
+    println("Хотите бросить кости еще раз? Введите Да или Нет")
+    val yesNo = readln().lowercase()
+    return yesNo
 }
