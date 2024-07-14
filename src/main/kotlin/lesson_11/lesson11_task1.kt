b7ab5780c2b367dd3c51ec4c985fac11c1296875
Maksim Medvedev <maksim.9678@mail.ru>
2024-07-14 22:40:11 +0300

    KS-11-1 version 1

---
+++ src/main/kotlin/lesson_11/lesson11_task1.kt
@@ -0,0 +1,31 @@
+package org.example.lesson_11
+
+
+fun main() {
+    val user1 = User(
+        id = 1,
+        login = "duego",
+        password = "d1234d",
+        mail = "diego@mail.ru",
+    )
+    val user2 = User(
+        id = 2,
+        login = "cima",
+        password = "c1234c",
+        mail = "cima@mail.ru",
+    )
+    println("""
+        ${user1.id}
+        ${user1.login}
+        ${user1.password}
+        ${user1.mail}
+    """.trimIndent()
+    )
+    println("""
+        ${user2.id}
+        ${user2.login}
+        ${user2.password}
+        ${user2.mail}
+    """.trimIndent()
+    )
+}
