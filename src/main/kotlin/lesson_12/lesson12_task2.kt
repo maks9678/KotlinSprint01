81c67a0332f5f1a00fbb0f494ef0ce9f13d76773
Maksim Medvedev <maksim.9678@mail.ru>
2024-07-31 23:27:21 +0300

    KS-12-2 version 2

---
+++ src/main/kotlin/lesson_12/lesson12_task2.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_12
+
+fun main() {
+    val dailyWeatherData1 = DailyWeatherDataV2(20, 15, true)
+    val dailyWeatherData2 = DailyWeatherDataV2(30, 5, false)
+    println(dailyWeatherData1.weatherPrintout())
+    println(dailyWeatherData2.weatherPrintout())
+}
+
+class DailyWeatherDataV2(
+    var daytimeTemperature: Int,
+    var nightTemperature: Int,
+    var isPrecipitation: Boolean,
+) {
+
+    fun weatherPrintout() {
+        println("$daytimeTemperature\n$nightTemperature\n$isPrecipitation")
+    }
+}
