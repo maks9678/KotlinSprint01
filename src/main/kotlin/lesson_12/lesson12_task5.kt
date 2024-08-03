afe57d2c6197a52fc2f97dacdde71199f84f60a7
Maksim Medvedev <maksim.9678@mail.ru>
2024-08-03 22:48:49 +0300

    KS-12-5 version 1

--- src/main/kotlin/lesson_12/lesson12_task5.kt
+++ src/main/kotlin/lesson_12/lesson12_task5.kt
@@ -5,48 +5,55 @@ import kotlin.random.Random
 
 const val KELVIN_TO_CELSIUS = 273
 fun main() {
-    val monthlyWeatherData: Array<Int> = arrayOf()
+    val monthlyWeatherData = mutableListOf<DailyWeatherDataV5>()
+
     for (i in 1..30) {
-        val daytimeTemperatureRandom:Int = (-15.. 36).random()
-        val nightTemperatureRandom:Int = (-15.. 20).random()
+        val daytimeTemperatureRandom: Int = (-15..36).random()
+        val nightTemperatureRandom: Int = (-15..20).random()
         val isPrecipitationRandom = Random.nextBoolean()
         monthlyWeatherData.add(
             DailyWeatherDataV5(
                 daytimeTemperatureRandom,
                 nightTemperatureRandom,
-                isPrecipitationRandom
+                isPrecipitationRandom,
             )
+        )
+    }
+    var monthlyWeatherDataDayTime = monthlyWeatherData.subList(0, 30)
+    var monthlyWeatherDataNight = monthlyWeatherData.subList(31, 60)
+    var monthlyWeatherDataPrecipitation = monthlyWeatherData.subList(61, 90)
+    val averageDaytimeTemperatures: Double = monthlyWeatherDataDayTime.average()
+    val averageNightTemperatures: Double = monthlyWeatherDataNight.average()
+    var daysWithPrecipitation: Int = 0
+    for (i in monthlyWeatherDataNight) {
+        if (i == "true") {
+            daysWithPrecipitation++
+        }
     }
 }
+    class DailyWeatherDataV5(
+        _daytimeTemperature: Int,
+        _nightTemperature: Int,
+        _isPrecipitation: Boolean,
+    ) {
+        var daytimeTemperature = _daytimeTemperature - KELVIN_TO_CELSIUS
+        var nightTemperature = _nightTemperature - KELVIN_TO_CELSIUS
+        var isPrecipitation = _isPrecipitation
+        fun weatherPrintout() {
+            println("$daytimeTemperature\n$nightTemperature\n$isPrecipitation")
+        }
 
-fun random(i: Int, i1: Int) {
-
-}
-
-class DailyWeatherDataV5(
-    _daytimeTemperature: Int,
-    _nightTemperature: Int,
-    _isPrecipitation: Boolean,
-) {
-    var daytimeTemperature = _daytimeTemperature - KELVIN_TO_CELSIUS
-    var nightTemperature = _nightTemperature - KELVIN_TO_CELSIUS
-    var isPrecipitation = _isPrecipitation
-    fun weatherPrintout() {
-        println("$daytimeTemperature\n$nightTemperature\n$isPrecipitation")
+        init {
+            weatherPrintout()
+        }
     }
-
-    init {
-        weatherPrintout()
-    }
-}
-//
-//Продолжи усовершенствовать программу, теперь
-// она должна рассчитывать средние значения температур и считать количество дней с осадками за месяц.
+//Продолжи усовершенствовать программу, теперь она должна рассчитывать средние значения
+// температур и считать количество дней с осадками за месяц.
 //
 //Представь, что данные ты получаешь от датчиков. Сымитируй их поведение с помощью random.
 //
-//- в функции main() создай список и добавь в него с помощью цикла 30 объектов класса,
-// передав в конструкторы случайные значения;
+//- в функции main() создай список и добавь в него с помощью цикла 30 объектов класса, передав в
+// конструкторы случайные значения;
 //- сохрани в отдельные списки дневные и ночные температуры, а также список дней с осадками;
 //- вычисли средние значения дневных и ночных температур с помощью специальной extension-функции
 // из стандартной библиотеки;
