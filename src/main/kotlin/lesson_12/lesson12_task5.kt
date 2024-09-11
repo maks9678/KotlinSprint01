cdce56d74c6508160ddd5c41f6d4f292ef297e3b
Maksim Medvedev <maksim.9678@mail.ru>
2024-09-12 00:33:20 +0300

    KS-12-5 version 1

--- src/main/kotlin/lesson_12/lesson12_task5.kt
+++ src/main/kotlin/lesson_12/lesson12_task5.kt
@@ -25,38 +25,30 @@ fun main() {
     val averageDaytimeTemperatures: Double = monthlyWeatherDataDayTime.average()
     val averageNightTemperatures: Double = monthlyWeatherDataNight.average()
     var daysWithPrecipitation: Int = 0
-    for (i in monthlyWeatherDataNight) {
-        if (i == "true") {
+    for (i in monthlyWeatherDataPrecipitation) {
+        if (i.toBoolean() == true) {
             daysWithPrecipitation++
         }
     }
-}
-    class DailyWeatherDataV5(
-        _daytimeTemperature: Int,
-        _nightTemperature: Int,
-        _isPrecipitation: Boolean,
-    ) {
-        var daytimeTemperature = _daytimeTemperature - KELVIN_TO_CELSIUS
-        var nightTemperature = _nightTemperature - KELVIN_TO_CELSIUS
-        var isPrecipitation = _isPrecipitation
-        fun weatherPrintout() {
-            println("$daytimeTemperature\n$nightTemperature\n$isPrecipitation")
-        }
 
-        init {
-            weatherPrintout()
-        }
+    println("Средняя дневная температура $averageDaytimeTemperatures")
+    println("Средняя ночная температура $averageNightTemperatures")
+    println("Количество дней с осадками $daysWithPrecipitation")
+}
+
+class DailyWeatherDataV5(
+    _daytimeTemperature: Int,
+    _nightTemperature: Int,
+    _isPrecipitation: Boolean,
+) {
+    var daytimeTemperature = _daytimeTemperature - KELVIN_TO_CELSIUS
+    var nightTemperature = _nightTemperature - KELVIN_TO_CELSIUS
+    var isPrecipitation = _isPrecipitation
+    fun weatherPrintout() {
+        println("$daytimeTemperature\n$nightTemperature\n$isPrecipitation")
     }
-//Продолжи усовершенствовать программу, теперь она должна рассчитывать средние значения
-// температур и считать количество дней с осадками за месяц.
-//
-//Представь, что данные ты получаешь от датчиков. Сымитируй их поведение с помощью random.
-//
-//- в функции main() создай список и добавь в него с помощью цикла 30 объектов класса, передав в
-// конструкторы случайные значения;
-//- сохрани в отдельные списки дневные и ночные температуры, а также список дней с осадками;
-//- вычисли средние значения дневных и ночных температур с помощью специальной extension-функции
-// из стандартной библиотеки;
-//- посчитай количество дней с осадками.
-//
-//Выведи результаты в консоль.
+
+    init {
+        weatherPrintout()
+    }
+}
