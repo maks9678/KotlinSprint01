71507a15994593586d4dd665a3b5908b4a7d29a7
Maksim Medvedev <maksim.9678@mail.ru>
2024-09-25 22:18:33 +0300

    KS-13-1 version 2

--- src/main/kotlin/lesson_13/lesson13_task1.kt
+++ src/main/kotlin/lesson_13/lesson13_task1.kt
@@ -1,8 +1,8 @@
 package org.example.lesson_13
 
 fun main() {
-    val phoneDirectory = PhoneDirectory(readln(), readln(), readln())
+    val phoneDirectory = PhoneDirectory(readln(), readln().toLong(), readln())
 }
 
-class PhoneDirectory(val name: String, val phoneNumber: String, val company: String?) {
+class PhoneDirectory(val name: String, val phoneNumber: Long, val company: String?) {
 }
