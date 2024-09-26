ff71c9c3c430800933364a366a4fba8302fb7ebd
Maksim Medvedev <maksim.9678@mail.ru>
2024-09-26 22:40:13 +0300

    KS-13-3 version 1

---
+++ src/main/kotlin/lesson_13/lesson13_task3.kt
@@ -0,0 +1,26 @@
+package org.example.lesson_13
+
+fun main() {
+    val phoneDirectory = mutableListOf(
+        PhoneDirectory3("Олег", 89782211221, "Тинькофф"),
+        PhoneDirectory3("null", 89782211222, null),
+        PhoneDirectory3("Влад", 89782211223, null),
+        PhoneDirectory3("Милана", 879782211224, "Яндекс"),
+        PhoneDirectory3("Гарик", 89782211225, "Reddit")
+    )
+    phoneDirectory.forEach { i -> i.printDirectory() }
+}
+
+class PhoneDirectory3(
+    val name: String,
+    val phoneNumber: Long,
+    val company: String? = null,
+) {
+    fun printDirectory() {
+        println(
+            "- Имя: $name\n" +
+                    "- Номер: $phoneNumber\n" +
+                    "- Компания: ${company ?: "не указано"}"
+        )
+    }
+}
