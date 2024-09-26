bdded6101697f97b18bdf1780f60ebcf71bfcc58
Maksim Medvedev <maksim.9678@mail.ru>
2024-09-26 12:50:02 +0300

    KS-13-2 version 2

--- src/main/kotlin/lesson_13/lesson13_task2.kt
+++ src/main/kotlin/lesson_13/lesson13_task2.kt
@@ -1,14 +1,11 @@
 package org.example.lesson_13
 
 fun main() {
-    val phoneDirectory = PhoneDirectory2(readln(), readln(), readln())
+    val phoneDirectory = PhoneDirectory2(readln(), readln().toLong(), readln())
     phoneDirectory.printDirectory()
 }
 
-class PhoneDirectory2(_name: String, _phoneNumber: String, val _company: String? = null) {
-    val name = _name
-    val phoneNumber = _phoneNumber
-    var company = _company ?: "не указано"
+class PhoneDirectory2(val name: String, val phoneNumber: Long, val company: String? = null ?: "не указано") {
     fun printDirectory() {
         println(
             "- Имя: $name\n" +
