b231efaff96ce8788f9099b9f05e76f8057cdb7b
Maksim Medvedev <maksim.9678@mail.ru>
2024-09-26 13:36:29 +0300

    master version 2

--- src/main/kotlin/lesson__1_10/lesson_10/lesson10_task1.kt
+++ src/main/kotlin/lesson__1_10/lesson_10/lesson10_task1.kt
@@ -1,15 +1,15 @@
 package org.example.lesson__1_10.lesson_10
 
-const val MIN_RANDOM_NUMBER = 1
-const val MAX_RANDOM_NUMBER = 6
+private const val MIN_RANDOM_NUMBER = 1
+private const val MAX_RANDOM_NUMBER = 6
 
 fun main() {
     println("Игрок бросает кости")
-    val playerResult: Int = rollOfTheDice()
+    val playerResult: Int = rollOfTheDice2()
     println("Игроку выпало: $playerResult ")
 
     println("Компьютер бросает кости")
-    val computerResult: Int = rollOfTheDice()
+    val computerResult: Int = rollOfTheDice2()
     println("Компьютеру выпало: $computerResult ")
 
     if (playerResult > computerResult) println("Победило человечество")
@@ -18,6 +18,6 @@ fun main() {
 
 }
 
-fun rollOfTheDice(): Int {
+fun rollOfTheDice2(): Int {
     return (MIN_RANDOM_NUMBER..MAX_RANDOM_NUMBER).random()
 }
