df7ce87ba404900dff0b6c66531d90a3fcf1745a
Maksim Medvedev <maksim.9678@mail.ru>
2024-10-02 00:39:46 +0300

    KS-11-5 version 1

--- src/main/kotlin/lesson_11/lesson11_task5.kt
+++ src/main/kotlin/lesson_11/lesson11_task5.kt
@@ -1,47 +1,49 @@
 package org.example.lesson_11
 
-fun main() {
-}
+class Forum {
+    val users = mutableListOf<User5>()
+    val messages = mutableListOf<Message>()
+    var userIdCounter = 1
+    var messageIdCounter = 1
 
-class Forum(
+    fun createNewUser(userName: String): User5 {
+        val user = User5(userIdCounter++, userName)
+        users.add(user)
+        return user
+    }
 
-) {
-    val forumPosts: MutableList<String> = mutableListOf()
+    fun createNewMessage(authorId: Int, message: String) {
+        if (users.any { it.userId == authorId }) {
+            val newMessage = Message(messageIdCounter++, authorId, message)
+            messages.add(newMessage)
+        } else {
+            println("Пользователь с id $authorId не найден.")
+        }
+    }
+
     fun printThread() {
-
+        for (message in messages) {
+            val authorName = users.find { it.userId == message.authorId }?.userName
+            if (authorName != null) {
+                println("$authorName: ${message.message}")
+            }
+        }
     }
 }
 
-class ForumMember(
-    val userId: Int,
-    val userName: String,
-) {
-    fun createNewUser(userName: String) {}
+class User5(val userId: Int, val userName: String) {}
+class Message(val messageId: Int, val authorId: Int, val message: String)
 
-}
+fun main() {
+    val forum = Forum()
 
-class ForumMessage(
-    val authorId: Int,
-    val message: String,
-) {
-    fun createNewMessage(userId: Int) {}
+    val user1 = forum.createNewUser("Alice")
+    val user2 = forum.createNewUser("Bob")
+
+    forum.createNewMessage(user1.userId, "Привет всем!")
+    forum.createNewMessage(user1.userId, "Как дела?")
+    forum.createNewMessage(user2.userId, "Привет, Алис!")
+    forum.createNewMessage(user2.userId, "Все отлично!")
+
+    forum.printThread()
 }
-//Реализовать класс работы форума (Forum).
-//Для реализации создай дополнительные классы для сущностей “Член форума”
-//(с полями userId и userName) и “Сообщение форума” (с полями authorId и message).
-//
-//Примени паттерн “Фабрика” для создания пользователей и сообщений форума.
-//Напрямую объекты не создаются, логика должна быть инкапсулирована внутри методов класса.
-//
-//- createNewUser() создает новых пользователей, принимая имя пользователя.
-//Метод сохраняет нового пользователя в общий список и одновременно
-//возвращает новый объект. Генерация id-шников новых пользователей происходит внутри;
-//- createNewMessage() создает сообщения, принимая id пользователя.
-//Сообщения создаются только если такой пользователь есть на форуме;
-//- printThread() печатает в консоль все сообщения добавленные на форум в формате:
-//
-//автор: сообщение
-//автор: сообщение
-//
-//Создай экземпляр Forum и продемонстрируй его работу, добавив двух пользователей
-//и по два сообщения от каждого пользователя.
