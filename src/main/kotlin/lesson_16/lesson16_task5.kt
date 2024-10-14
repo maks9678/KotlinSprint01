77e018980e68f3a62a9b843ecc90ab51e62c1554
Maksim Medvedev <maksim.9678@mail.ru>
2024-10-14 20:13:17 +0300

    KS-16-4 version 1

---
+++ src/main/kotlin/lesson_16/lesson16_task5.kt
@@ -0,0 +1,39 @@
+package org.example.lesson_16
+
+fun main() {
+    val player1 = Player(name = "Воин", health = 100, impactForce = 10)
+    val player2 = Player(name = "Маг", health = 80, impactForce = 15)
+
+    player1.takingDamage(30)
+    player2.takingDamage(50)
+    player1.healing(20)
+    player1.takingDamage(100)
+    player1.healing(10)
+}
+
+class Player(
+    val name: String,
+    private var health: Int,
+    var impactForce: Int,
+) {
+    fun takingDamage(damage: Int) {
+        if (health > 0) {
+            if (damage >= health) death()
+            health -= damage
+            println("игрок $name получил $damage урона")
+        } else println("игрок мертв")
+    }
+
+    fun healing(point: Int) {
+        if (health > 0) {
+            health += point
+            println("игрок $name восполнил $point здоровья")
+        } else println("игрок мертв")
+    }
+
+    private fun death() {
+        impactForce = 0
+        health = 0
+        println("игрок $name умер")
+    }
+}
