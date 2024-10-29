e032b7aeef9f0af48a65016cc1bd20d62c122972
Maksim Medvedev <maksim.9678@mail.ru>
2024-10-29 23:27:25 +0300

    KS-20-1 version 2

--- src/main/kotlin/lesson_20/lesson20_task1.kt
+++ src/main/kotlin/lesson_20/lesson20_task1.kt
@@ -1,7 +1,7 @@
 package org.example.lesson_20
 
-fun main (){
+fun main() {
     val userName1 = "Иван"
-    val splashScreen:(String)->Unit ={username-> println("С наступающим Новым Годом, $username!")}
+    val splashScreen: (String) -> Unit = { username -> println("С наступающим Новым Годом, $username!") }
     splashScreen(userName1)
 }
