3d12fd65c6e9b56ca0a2bde770adc369000a7d03
Maksim Medvedev <maksim.9678@mail.ru>
2024-10-30 22:50:20 +0300

    KS-20-5 version 1

---
+++ src/main/kotlin/lesson_20/lesson20_task5.kt
@@ -0,0 +1,30 @@
+package org.example.lesson_20
+
+fun main() {
+    val robot1 = Robot()
+    robot1.say()
+    robot1.setModifier { phrase -> invert(phrase) }
+    robot1.say()
+}fun invert(phrase: String): String {
+    return phrase.split("").reversed().joinToString { " " }
+
+}
+class Robot() {
+    var modifier: (String) -> String = { it }
+    val phrases = listOf(
+        "Привет, мир!",
+        "Как дела?",
+        "Сегодня прекрасный день!",
+        "Я робот!",
+        "Выполняю задачи."
+    )
+
+    fun say() {
+        val phrasesPrint = phrases.random()
+        println(modifier(phrasesPrint))
+    }
+
+    fun setModifier(newMedifier: (String) -> String) {
+        modifier = newMedifier
+    }
+}
