d5b104493d51427276b8a5135d2a44925d482c79
Maksim Medvedev <maksim.9678@mail.ru>
2024-11-15 21:16:23 +0300

    KS-21-4 version 1

--- src/main/kotlin/lesson_21/lesson21_task4.kt
+++ src/main/kotlin/lesson_21/lesson21_task4.kt
@@ -1,10 +1,13 @@
 package org.example.lesson_21
 
-//В курсовой работе мы разрабатываем телеграм-бота на Kotlin,
-//который помогает пользователям учить иностранные слова.
-//Создай extension функцию для класса File из стандартной библиотеки,
-//которая будет записывать переданное ей слово в файл.
-//
-//- дополнительные материалы про файлы есть в 1-2 этапах курсовой;
-//- функция-расширение должна записывать текст в нижнем регистре;
-//- функция-расширение должна записывать текст в начало файла.
+fun main() {
+    val text = File("text.txt")
+    text.writeText("")
+    text.writeDownWord("красный")
+}
+
+fun File.writeDownWord(_word: String) {
+    val word = _word.lovercase()
+    val currentContent = this.readText()
+    this.writeText("$word\n $currentContent")
+}
