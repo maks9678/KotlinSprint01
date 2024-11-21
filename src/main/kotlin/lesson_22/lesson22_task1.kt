ee70b10d1e1a3b296d8c18b13593b9fd663e8d72
Maksim Medvedev <maksim.9678@mail.ru>
2024-11-21 18:40:53 +0300

    KS-22-1 version 1

--- src/main/kotlin/lesson_22/lesson22_task1.kt
+++ src/main/kotlin/lesson_22/lesson22_task1.kt
@@ -1,12 +1,14 @@
 package org.example.lesson_22
 
-fun main() {
-    val book1 = RegularBook("Война и мир", "Толстой")
-    val book2 = RegularBook("Анна Каренина", "Толстой")
-    val book3 = DataBook("Бесы", "Достоевский")
-    val book4 = DataBook("Идиот", "Достоевский")
-
-}
-
 class RegularBook(val title: String, val author: String)
 data class DataBook(val title: String, val author: String)
+
+fun main() {
+    val book1 = RegularBook("Война и мир", "Толстой")
+    val book2 = RegularBook("Война и мир", "Толстой")
+    val book3 = DataBook("Война и мир", "Толстой")
+    val book4 = DataBook("Война и мир", "Толстой")
+    println(book1 == book2)//false, тк сравниваются ссылки, а не значения
+    println(book3 == book4)//true, тк сравниваются значения свойств
+    println(book1.equals(book3))//false, тк разные классы
+}
